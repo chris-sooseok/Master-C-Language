b3a8c0fee4a952d6e64aa9aea2f853e32de9c844
chris-sooseok <sooseokkim99@gmail.com>
2024-09-28 16:03:30 -0400

    Section 4 done. More notes about preprocessor

--- Project1/Project1/link_error.c
+++ Project1/Project1/link_error.c
@@ -15,15 +15,18 @@ int main() {
     
     return 0;
 }
-
-// When building this code errors occur:
-// error1 [ Undefined symbol: x ]
-// error2 [ Linekr command failed ]
+/*
+When building this code errors occur:
+error1 [ Undefined symbol: x ]
+error2 [ Linekr command failed ]
+ */
 
 // This is not a compiler error; this code will compile very fine
 
-// The process is
-// 1. The compiler produces linker_error.o file
-// 2. Linker will combine linker_error.o file with stdio.h library
-// 3. Since there is external x, which means the x variable comes from a diffferent source, the linker will try to fetch the x as well
-// 4. Since we don't have any source file that defines x, we encounter an error for this
+/*
+The process is
+1. The compiler produces linker_error.o file
+2. Linker will combine linker_error.o file with stdio.h library
+3. Since there is external x, which means the x variable comes from a diffferent source, the linker will try to fetch the x as well
+4. Since we don't have any source file that defines x, we encounter an error for this
+*/
--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # C Programming For Beginners
 
 
-
 This is a repository dedicated to learning C Programming Language
+
 
 IDE Used: Xcode <br>
 https://www.youtube.com/watch?v=fwkNQVdwiAA
 
 
-The course link is below <br>
-Link: https://www.udemy.com/course/c-programming-for-beginners-/?couponCode=ST11MT91624B
+The course link is <br>
+ https://www.udemy.com/course/c-programming-for-beginners-/?couponCode=ST11MT91624B
